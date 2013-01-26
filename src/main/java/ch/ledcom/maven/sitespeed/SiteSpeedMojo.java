cac9e52d5684d571fd911102ba775bef82a75ac4
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-01-26 18:24:18 +0100

    Added support for all current sitespeed options

--- src/main/java/ch/ledcom/maven/sitespeed/SiteSpeedMojo.java
+++ src/main/java/ch/ledcom/maven/sitespeed/SiteSpeedMojo.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -27,6 +26,8 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.Closeables;
 
 /**
@@ -40,21 +41,71 @@ import com.google.common.io.Closeables;
 @Mojo(name = "sitespeed")
 public class SiteSpeedMojo extends AbstractMojo {
 
+    private static final String PROPERTY_PREFIX = "siteSpeed";
+
     /** Path to the SiteSpeed.io directory. */
-    @Parameter(property = "siteSpeed.siteSpeePath", required = true)
+    @Parameter(property = PROPERTY_PREFIX + ".siteSpeedPath", required = true)
     private File siteSpeedPath;
 
     /** Path to the PhantomJS directory. */
-    @Parameter(property = "siteSpeed.phantomJSPath", required = true)
+    @Parameter(property = PROPERTY_PREFIX + ".phantomJSPath", required = true)
     private File phantomJSPath;
 
     /** The start url for the test. */
-    @Parameter(property = "siteSpeed.url", required = true)
+    @Parameter(property = PROPERTY_PREFIX + ".url", required = true)
     private URL url;
 
+    /** Crawl depth, default is 1. */
+    @Parameter(property = PROPERTY_PREFIX + ".crawlDepth", required = false, defaultValue = "1")
+    private int crawlDepth;
+
+    /** Skip urls that contains this in the path. */
+    @Parameter(property = PROPERTY_PREFIX + ".skipUrls", required = false)
+    private String skipUrls;
+
+    /** The number of processes that will analyze pages. */
+    @Parameter(property = PROPERTY_PREFIX + ".nbProcesses", required = false, defaultValue = "5")
+    private int nbProcesses;
+
+    /** The memory heap size for the java applications. */
+    @Parameter(property = PROPERTY_PREFIX + ".maxHeap", required = false, defaultValue = "1024")
+    private int maxHeap;
+
+    /** The output format, always output as html but you can add images (img). */
+    @Parameter(property = PROPERTY_PREFIX + ".outputFormat", required = false)
+    private String outputFormat;
+
     /** The result base directory. */
-    @Parameter(property = "siteSpeed.outputDir", required = false, defaultValue = "target/sitespeed-result")
+    @Parameter(property = PROPERTY_PREFIX + ".outputDir", required = false, defaultValue = "${project.build.directory}/sitespeed-result")
     private File outputDir;
+
+    /** Create a tar zip file of the result files. */
+    @Parameter(property = PROPERTY_PREFIX + ".zip", required = false, defaultValue = "false")
+    private boolean zip;
+
+    /** The proxy host & protocol: proxy.soulgalore.com:80. */
+    @Parameter(property = PROPERTY_PREFIX + ".proxy", required = false)
+    private String proxy;
+
+    /** The proxy type. */
+    @Parameter(property = PROPERTY_PREFIX + ".proxyType", required = false, defaultValue = "http")
+    private String proxyType;
+
+    /** The user agent. */
+    @Parameter(property = PROPERTY_PREFIX + ".userAgent", required = false, defaultValue = "Mozilla/6.0")
+    private String userAgent;
+
+    /** The view port, the page viewport size WidthxHeight, like 400x300. */
+    @Parameter(property = PROPERTY_PREFIX + ".viewPort", required = false, defaultValue = "1280x800")
+    private String viewport;
+
+    /** The compiled yslow file. */
+    @Parameter(property = PROPERTY_PREFIX + ".yslowFile", required = false, defaultValue = "dependencies/yslow-3.1.4-sitespeed.js")
+    private String yslowFile;
+
+    /** Which ruleset to use, default is the latest sitespeed.io version. */
+    @Parameter(property = PROPERTY_PREFIX + ".rulesetVersion", required = false)
+    private String rulesetVersion;
 
     /**
      * Main Mojo method.
@@ -67,17 +118,13 @@ public class SiteSpeedMojo extends AbstractMojo {
     @Override
     public final void execute() throws MojoExecutionException,
             MojoFailureException {
-        getLog().info("siteSpeedPath=[" + siteSpeedPath + "]");
-        getLog().info("phantomJSPath=[" + phantomJSPath + "]");
-        getLog().info("url=[" + url.toExternalForm() + "]");
-        getLog().info("outputDir=[" + outputDir + "]");
+        logParameters();
         BufferedReader reader = null;
         boolean threw = true;
         try {
             ProcessBuilder pb = new ProcessBuilder(constructCmdarray());
             pb.directory(siteSpeedPath);
-            pb.environment().put("PATH",
-                    getPhantomJSBinDir() + ":" + pb.environment().get("PATH"));
+            enrichEnvironment(pb);
             pb.redirectErrorStream(true);
             Process p = pb.start();
             reader = new BufferedReader(new InputStreamReader(
@@ -86,7 +133,11 @@ public class SiteSpeedMojo extends AbstractMojo {
             while ((line = reader.readLine()) != null) {
                 getLog().info(line);
             }
-            p.waitFor();
+            int status = p.waitFor();
+            if (status != 0) {
+                throw new MojoExecutionException(
+                        "sitespeed.io exited with status [" + status + "]");
+            }
             threw = false;
         } catch (IOException ioe) {
             throw new MojoExecutionException(
@@ -104,19 +155,66 @@ public class SiteSpeedMojo extends AbstractMojo {
         }
     }
 
+    private void logParameters() {
+        getLog().info("siteSpeedPath=[" + siteSpeedPath + "]");
+        getLog().info("phantomJSPath=[" + phantomJSPath + "]");
+        getLog().info("url=[" + url.toExternalForm() + "]");
+        getLog().info("crawlDepth=[" + crawlDepth + "]");
+        getLog().info("skipUrls=[" + skipUrls + "]");
+        getLog().info("nbProcesses=[" + nbProcesses + "]");
+        getLog().info("maxHeap=[" + maxHeap + "]");
+        getLog().info("outputFormat=[" + outputFormat + "]");
+        getLog().info("outputDir=[" + outputDir + "]");
+        getLog().info("zip=[" + zip + "]");
+        getLog().info("proxy=[" + proxy + "]");
+        getLog().info("proxyType=[" + proxyType + "]");
+        getLog().info("userAgent=[" + userAgent + "]");
+        getLog().info("viewport=[" + viewport + "]");
+        getLog().info("yslowFile=[" + yslowFile + "]");
+        getLog().info("rulesetVersion=[" + rulesetVersion + "]");
+    }
+
+    private void enrichEnvironment(ProcessBuilder pb) {
+        final String currentPath = pb.environment().get("PATH");
+        pb.environment().put("PATH", getPhantomJSBinDir() + ":" + currentPath);
+    }
+
     /**
      * Construct the sitespeed.io command.
      * 
      * @return the command to run (including arguments)
      */
     private List<String> constructCmdarray() {
-        List<String> cmd = new ArrayList<String>();
-        cmd.add(getSiteSpeedBin().toString());
-        cmd.add("-u");
-        cmd.add(url.toExternalForm());
-        cmd.add("-r");
-        cmd.add(outputDir.toString());
-        return cmd;
+        ImmutableList.Builder<String> builder = ImmutableList
+                .<String> builder();
+        builder.add(getSiteSpeedBin().toString());
+        builder.add("-u").add(url.toExternalForm());
+        builder.add("-d").add(Integer.toString(crawlDepth));
+        if (!Strings.isNullOrEmpty(skipUrls)) {
+            builder.add("-f").add(skipUrls);
+        }
+        builder.add("-p").add(Integer.toString(nbProcesses));
+        builder.add("-m").add(Integer.toString(maxHeap));
+        if (!Strings.isNullOrEmpty(outputFormat)) {
+            builder.add("-o").add(outputFormat);
+        }
+        builder.add("-r").add(outputDir.toString());
+        if (zip) {
+            builder.add("-z");
+        }
+        if (!Strings.isNullOrEmpty(proxy)) {
+            builder.add("-x").add(proxy);
+        }
+        if (!Strings.isNullOrEmpty(proxyType)) {
+            builder.add("-t").add(proxyType);
+        }
+        builder.add("-a").add(userAgent);
+        builder.add("-v").add(viewport);
+        builder.add("-y").add(yslowFile);
+        if (!Strings.isNullOrEmpty(rulesetVersion)) {
+            builder.add("-l").add(rulesetVersion);
+        }
+        return builder.build();
     }
 
     /**
