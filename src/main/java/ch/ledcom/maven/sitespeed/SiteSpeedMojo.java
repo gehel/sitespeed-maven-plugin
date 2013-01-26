fe53f805b35d3923145b1587a08f21fc8a01e0f4
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-01-26 14:28:56 +0100

    minor code cleanup

--- src/main/java/ch/ledcom/maven/sitespeed/SiteSpeedMojo.java
+++ src/main/java/ch/ledcom/maven/sitespeed/SiteSpeedMojo.java
@@ -27,65 +27,106 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import com.google.common.io.Closeables;
+
+/**
+ * Generate a SiteSpeed.io report.
+ * 
+ * At the moment this Mojo is a simple wrapper around the sitespeed.io bash
+ * script.
+ * 
+ * @author gehel
+ */
 @Mojo(name = "sitespeed")
 public class SiteSpeedMojo extends AbstractMojo {
 
-	@Parameter(property = "siteSpeed.siteSpeePath", required = true)
-	private File siteSpeedPath;
+    /** Path to the SiteSpeed.io directory. */
+    @Parameter(property = "siteSpeed.siteSpeePath", required = true)
+    private File siteSpeedPath;
 
-	@Parameter(property = "siteSpeed.phantomJSPath", required = true)
-	private File phantomJSPath;
+    /** Path to the PhantomJS directory. */
+    @Parameter(property = "siteSpeed.phantomJSPath", required = true)
+    private File phantomJSPath;
 
-	@Parameter(property = "siteSpeed.url", required = true)
-	private URL url;
+    /** The start url for the test. */
+    @Parameter(property = "siteSpeed.url", required = true)
+    private URL url;
 
-	@Parameter(property = "siteSpeed.outputDir", required = false, defaultValue = "target/sitespeed-result")
-	private File outputDir;
+    /** The result base directory. */
+    @Parameter(property = "siteSpeed.outputDir", required = false, defaultValue = "target/sitespeed-result")
+    private File outputDir;
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("siteSpeedPath=[" + siteSpeedPath + "]");
-		getLog().info("phantomJSPath=[" + phantomJSPath + "]");
-		getLog().info("url=[" + url.toExternalForm() + "]");
-		getLog().info("outputDir=[" + outputDir + "]");
-		try {
-			ProcessBuilder pb = new ProcessBuilder(constructCmdarray());
-			pb.directory(siteSpeedPath);
-			pb.environment().put("PATH",
-					getPhantomJSBinDir() + ":" + pb.environment().get("PATH"));
-			pb.redirectErrorStream(true);
-			Process p = pb.start();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					p.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				getLog().info(line);
-			}
-			p.waitFor();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Main Mojo method.
+     * 
+     * @throws MojoExecutionException
+     *             in case of execution error
+     * @throws MojoFailureException
+     *             in case of execution failure
+     */
+    @Override
+    public final void execute() throws MojoExecutionException,
+            MojoFailureException {
+        getLog().info("siteSpeedPath=[" + siteSpeedPath + "]");
+        getLog().info("phantomJSPath=[" + phantomJSPath + "]");
+        getLog().info("url=[" + url.toExternalForm() + "]");
+        getLog().info("outputDir=[" + outputDir + "]");
+        BufferedReader reader = null;
+        try {
+            ProcessBuilder pb = new ProcessBuilder(constructCmdarray());
+            pb.directory(siteSpeedPath);
+            pb.environment().put("PATH",
+                    getPhantomJSBinDir() + ":" + pb.environment().get("PATH"));
+            pb.redirectErrorStream(true);
+            Process p = pb.start();
+            reader = new BufferedReader(new InputStreamReader(
+                    p.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                getLog().info(line);
+            }
+            p.waitFor();
+        } catch (IOException ioe) {
+            throw new MojoExecutionException(
+                    "IOException when generating report", ioe);
+        } catch (InterruptedException ie) {
+            throw new MojoExecutionException(
+                    "InterruptedException when generating report", ie);
+        } finally {
+            Closeables.closeQuietly(reader);
+        }
+    }
 
-	private List<String> constructCmdarray() {
-		List<String> cmd = new ArrayList<String>();
-		cmd.add(getSiteSpeedBin().toString());
-		cmd.add("-u");
-		cmd.add(url.toExternalForm());
-		cmd.add("-r");
-		cmd.add(outputDir.toString());
-		return cmd;
-	}
+    /**
+     * Construct the sitespeed.io command.
+     * 
+     * @return the command to run (including arguments)
+     */
+    private List<String> constructCmdarray() {
+        List<String> cmd = new ArrayList<String>();
+        cmd.add(getSiteSpeedBin().toString());
+        cmd.add("-u");
+        cmd.add(url.toExternalForm());
+        cmd.add("-r");
+        cmd.add(outputDir.toString());
+        return cmd;
+    }
 
-	private File getPhantomJSBinDir() {
-		return new File(phantomJSPath, "bin");
-	}
+    /**
+     * Get the <code>bin</code> directory of the PhantomJS distribution.
+     * 
+     * @return the PhantomJS <code>bin</code> directory
+     */
+    private File getPhantomJSBinDir() {
+        return new File(phantomJSPath, "bin");
+    }
 
-	private File getSiteSpeedBin() {
-		return new File(siteSpeedPath, "sitespeed.io");
-	}
+    /**
+     * Get the <code>sitespeed.io</code> script.
+     * 
+     * @return the <code>sitespeed.io</code> script
+     */
+    private File getSiteSpeedBin() {
+        return new File(siteSpeedPath, "sitespeed.io");
+    }
 }
